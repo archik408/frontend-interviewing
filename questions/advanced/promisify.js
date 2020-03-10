7d894974dd12ab63cbc377d144ab54209e5d3a1f
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-10 23:18:54 +0300

    Update promisify.js

--- questions/advanced/promisify.js
+++ questions/advanced/promisify.js
@@ -3,10 +3,15 @@
 // The callback will always have this signature:
 //   function(result){}
 
-/*
-    const exampleFn = function (x, y, callback) {};
+const exampleFn = function (x, y, callback) { setTimeout(() => callback(x + y), 100) };
 
-    const promisedFn = promisify(exampleFn);
+function promisify(fn) {
+  return (...args) => new Promise((resolve, reject) => {
+    // no need try {} catch (e) {} because Promise handle this case by default
+    // so, added check only for error-first callback case
+    fn && fn(...args, (result, ...rest) => result instanceof Error ? reject(result) : resolve(result, ...rest));
+  });
+}
 
-    promisedFn().then(...).then(...)
-*/
+const promisedFn = promisify(exampleFn);
+promisedFn(1, 3).then(r => r + 2).then(r => console.log(r)).catch(e => console.log(e.message)); // 6
