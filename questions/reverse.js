7b7cd587b57b4032344447792673a736f96e415d
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-10 23:32:28 +0300

    Update reverse.js

--- questions/reverse.js
+++ questions/reverse.js
@@ -3,3 +3,18 @@
 /*
     reverse('Hello, world!'); // !dlrow ,olleH
 */
+
+function reverse(text) {
+    // without error handling
+    return text.split('').reverse().join('');
+}
+
+// boring example
+function reverse2(text) {
+  let result = '';
+  for (let i = text.length - 1; i >= 0; i--)  {
+    result = result + text[i];
+  }
+  
+  return result;
+}
