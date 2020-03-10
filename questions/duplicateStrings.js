d6e2632c154c09734ddbb14028a2cbec1550cbc4
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-10 23:57:34 +0300

    Update duplicateStrings.js

--- questions/duplicateStrings.js
+++ questions/duplicateStrings.js
@@ -5,3 +5,33 @@
  const str = 'This is is a test test string';
  removeDuplicates(str); // 'This is a test string'
  */
+
+// optimized
+function removeDuplicates(text = '') {
+  // have no rules about '.', ',' and etc. (including double space) just skip this cases
+  const uniqMap = text
+    .split(' ')
+    .reduce((map, word) => {
+      if (!map[word]) map[word] = true;
+      return map;
+    }, {});
+    return Object.keys(uniqMap).join(' ');
+}
+
+// normal
+function removeDuplicates2(text = '') {
+  // have no rules about '.', ',' and etc. (including double space) just skip this cases
+  return text
+    .split(' ')
+    .reduce((acc, word) => {
+      if (!acc.includes(word)) acc.push(word);
+      return acc;
+    }, [])
+    .join(' ');
+}
+
+// hacky
+function removeDuplicates(text = '') {
+  // have no rules about '.', ',' and etc. (including double space) just skip this cases
+  return Array.from(new Set(text.split(' '))).join(' ');
+}
