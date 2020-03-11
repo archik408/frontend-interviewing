eea288ac78647c60395604ae6f5de80cb4db8f72
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-11 22:22:51 +0300

    Update flatten.js

--- questions/flatten.js
+++ questions/flatten.js
@@ -1,4 +1,17 @@
 // Without using .flat(), create a function to flatten an array
+const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
 
-// const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
-// flatten(exampleArray); // [1,2,3,4,5,6,7,8,9,10]
+function flatten(list = []) {
+  const result = [];
+  for (var i = 0; i < list.length; i++) {
+    const item = list[i];
+    if (Array.isArray(item)) {
+      result.push(...flatten(item));
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+console.log(flatten(exampleArray)); // [1,2,3,4,5,6,7,8,9,10]
