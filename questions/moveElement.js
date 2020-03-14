78087eac96104a3fc798d80e2ece66c66d9fb20b
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-14 23:41:25 +0300

    Update moveElement.js

--- questions/moveElement.js
+++ questions/moveElement.js
@@ -1,6 +1,19 @@
 // Create a function to move an element. The function arguments are,
 // distance, duration, and the element to move.
 
-/*
-    function moveElement(duration, distance, element) {}
-*/
+   const move = (duration, distance, el) => {
+      el.style.position = 'absolute';
+      const stepPerFrame = distance / (duration * 60);
+      console.log(stepPerFrame);
+      let position = 0;
+      const step = () => {
+        position = position + stepPerFrame;
+        el.style.transform = `translateX(${Math.round(position)}px)`;
+        console.log(position < distance, position);
+        if (position < distance) requestAnimationFrame(step);
+      }
+      requestAnimationFrame(step);
+    }
+    
+    window.onload = () => move(0.3, 100, document.querySelector('#str1')); 
+   
