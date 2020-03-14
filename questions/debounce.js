e1e0014d8f3ce12c64ac3119677b3c3aa0047155
Artur Basak <artur.basak.devingrodno@gmail.com>
2020-03-14 17:19:27 +0300

    Update debounce.js

--- questions/debounce.js
+++ questions/debounce.js
@@ -1,1 +1,16 @@
 // Implement debounce
+
+    const debounce = function(ff, delay) {
+      let working = false;
+
+      return (...args) => {
+
+        if (!working) {
+          working = true;
+          ff(...args);
+          setTimeout(() => {
+            working = false;
+          }, delay);
+        }
+      }
+    }
